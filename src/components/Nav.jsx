c4e1494b285923c53dba5531fd76b4cc65d2aec1
Anais Mateus <75923327+anaiscmateus@users.noreply.github.com>
2024-04-01 12:45:43 -0400

    add routing, add pages, add dist

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,29 +1,47 @@
 export default function Nav() {
-    return(
-        <>
-        <div className="grid grid-cols-4 font-raleway">
-            <div className="border border-black p-2">
-                <h4 className="font-medium font-anon">About</h4>
-                <a href="#" className="block text-sm hover:underline">Why</a>
-                <a href="#" className="block text-sm hover:underline">Team</a>
-                <a href="#" className="text-sm hover:underline">Support Our Work</a>
-            </div>
-            <div className="border border-black p-2">
-                <h4 className="font-medium font-anon hover:underline">Projects</h4>
-                <a href="#" className="block text-sm hover:underline">Portfolio</a>
-            </div>
-            <div className="border border-black p-2">
-                <h4 className="font-medium font-anon">Community</h4>
-                <a href="#" className="block text-sm hover:underline">Discord</a>
-                <a href="#" className="block text-sm hover:underline">Upcoming Events</a>
-            </div>
-            <div className="border border-black p-2">
-                <h4 className="font-medium font-anon">Collaborate</h4>
-                <a href="#" className="block text-sm hover:underline">Self Assessment</a>
-                <a href="#" className="block text-sm hover:underline">How It Works</a>
-                <a href="#" className="text-sm hover:underline">Contact</a>
-            </div>
+  return (
+    <>
+      <div className="grid grid-cols-4 font-raleway">
+        <div className="border border-black p-2">
+          <h4 className="font-medium font-anon">About</h4>
+          <a href="/why" className="block text-sm hover:underline">
+            Why
+          </a>
+          <a href="/team" className="block text-sm hover:underline">
+            Team
+          </a>
+          <a href="#" className="text-sm hover:underline">
+            Support Our Work
+          </a>
         </div>
-        </>
-    )
+        <div className="border border-black p-2">
+          <h4 className="font-medium font-anon">Projects</h4>
+          <a href="/portfolio" className="block text-sm hover:underline">
+            Portfolio
+          </a>
+        </div>
+        <div className="border border-black p-2">
+          <h4 className="font-medium font-anon">Community</h4>
+          <a href="#" className="block text-sm hover:underline">
+            Discord
+          </a>
+          <a href="#" className="block text-sm hover:underline">
+            Upcoming Events
+          </a>
+        </div>
+        <div className="border border-black p-2">
+          <h4 className="font-medium font-anon">Collaborate</h4>
+          <a href="/assessment" className="block text-sm hover:underline">
+            Self Assessment
+          </a>
+          <a href="/how" className="block text-sm hover:underline">
+            How It Works
+          </a>
+          <a href="/contact" className="text-sm hover:underline">
+            Contact
+          </a>
+        </div>
+      </div>
+    </>
+  );
 }
---
+++ src/pages/Why.jsx
@@ -0,0 +1,7 @@
+export default function Why() {
+    return(
+        <>
+            <p>This is the why page</p>
+        </>
+    )
+}
